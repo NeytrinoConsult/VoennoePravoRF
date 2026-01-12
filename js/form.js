988388af0e44255e8ba514af69e9f5baa52bf643
Lawer <daniilrudnev2000@gmail.com>
2026-01-12 19:56:29 +0300

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,1 +1,20 @@
+const FORM_URL = "URL_ВЕБ_ПРИЛОЖЕНИЯ";
+let consent = false;
+
+document.querySelector("form").addEventListener("submit", e => {
+  e.preventDefault();
+  document.getElementById("consentModal").style.display = "flex";
+});
+
+function acceptConsent() {
+  consent = true;
+  document.getElementById("consentModal").style.display = "none";
+
+  const form = document.getElementById("requestForm");
+  const data = new FormData(form);
+
+  fetch(FORM_URL, { method: "POST", body: data })
+    .then(() => alert("Заявка отправлена"))
+    .then(() => form.reset());
+}
 
